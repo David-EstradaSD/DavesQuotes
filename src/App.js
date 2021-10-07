f50f70ad039d9f4c11d021418913f045841d9d49
David <destrada2193@gmail.com>
2021-10-06 20:17:59 -0700

    Import Route, Switch from react-router in App.js:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+
 function App() {
   return (
     <div>
+      <Switch>
+        <Route>
+          
+        </Route>
+        <Route>
+
+        </Route>
+        <Route>
+
+        </Route>
+      </Switch>
 
     </div>
   );
