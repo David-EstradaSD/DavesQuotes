fe390f6cedfb65f72693db67cd1fc4db52da5e3d
David <destrada2193@gmail.com>
2021-10-07 18:29:53 -0700

    Implement useHistory for progrmmatic navigation when adding newQuote

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,9 +1,17 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../quotes/QuoteForm';
 
 const NewQuote = () => {
 
+    const history = useHistory(); // gives us our Browser History
+
     const addQuoteHandler = quoteData => {
         console.log(quoteData); // will send to DB later
+
+        history.push('/quotes'); 
+        // push() -- adds a new page on the stack of pages (allows user to go back to previous page)
+        // replace() -- like a 'redirect' where we change the page 
     };
 
     return (
