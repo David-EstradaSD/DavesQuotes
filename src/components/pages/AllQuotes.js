2e0f400473c2b28caed1a5ce1982d8919d9e26d9
David <destrada2193@gmail.com>
2021-10-08 11:38:35 -0700

    Remove DUMMY_QUOTES and add DB httpRequests

--- src/components/pages/AllQuotes.js
+++ src/components/pages/AllQuotes.js
@@ -1,16 +1,53 @@
-import QuoteList from '../quotes/QuoteList';
+import { useEffect } from "react";
+import QuoteList from "../quotes/QuoteList";
+import useHttp from "../../hooks/use-http";
+import { getAllQuotes } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import NoQuotesFound from '../quotes/NoQuotesFound';
 
-const DUMMY_QUOTES = [
-  { id: 'q1', author: 'David Estrada', text: "Adaptability is the key to success" },
-  { id: 'q2', author: 'Buddha', text: "Success is not the key to happiness, happiness is the key to success" },
-  { id: 'q3', author: 'Master Yoda', text: "Do or do not. There is not try" },
-];
-
+// const DUMMY_QUOTES = [
+//   {
+//     id: "q1",
+//     author: "David Estrada",
+//     text: "Adaptability is the key to success",
+//   },
+//   {
+//     id: "q2",
+//     author: "Buddha",
+//     text: "Success is not the key to happiness, happiness is the key to success",
+//   },
+//   { id: "q3", author: "Master Yoda", text: "Do or do not. There is not try" },
+// ];
 
 const AllQuotes = () => {
-  return (
-    <QuoteList quotes={DUMMY_QUOTES} />
-  );
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>;
+  }
+
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
 export default AllQuotes;
