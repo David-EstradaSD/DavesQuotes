07e2b39f5acee2bd0418be34ffd352b73f0c88ab
David <destrada2193@gmail.com>
2021-10-08 11:30:00 -0700

    Begin implementing Data persistence

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,22 +1,28 @@
-import { useHistory } from 'react-router-dom';
-
-import QuoteForm from '../quotes/QuoteForm';
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
+import QuoteForm from "../quotes/QuoteForm";
+import useHttp from "../../hooks/use-http";
+import { addQuote } from "../../lib/api";
 
 const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
+  const history = useHistory(); // gives us our Browser History
 
-    const history = useHistory(); // gives us our Browser History
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
 
-    const addQuoteHandler = quoteData => {
-        console.log(quoteData); // will send to DB later
+  const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData);
 
-        history.push('/quotes'); 
-        // push() -- adds a new page on the stack of pages (allows user to go back to previous page)
-        // replace() -- like a 'redirect' where we change the page 
-    };
+    history.push("/quotes");
+    // push() -- adds a new page on the stack of pages (allows user to go back to previous page)
+    // replace() -- like a 'redirect' where we change the page
+  };
 
-    return (
-        <QuoteForm onAddQuote={addQuoteHandler} />
-    );
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
